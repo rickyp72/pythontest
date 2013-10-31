299d8e6905639f7491f744fc2fd23773cbf7a8c3
ricky <rickyp72@googmail.com>
2013-10-31 15:30:28 +0000

    added new files

--- bannergrabber.py
+++ bannergrabber.py
@@ -11,29 +11,42 @@ def retBanner(ip, port):
         return
         
 def checkVulns(banner):
-     if ("FreeFloat Ftp Server (Version 1.00)" in ans):
+     if ("FreeFloat Ftp Server (Version 1.00)" in banner):
          print "[+] FreeFloat FTP Server is vulnerable."
-     elif ("tnftpd " in banner):
+     elif ("/tnftpd/ " in banner):
          print "[+] tnftpd FTP Server is vulnerable."
      else:
          print "[-] FTP Server isn't vulnerable."       
      return 
         
-def main():
-    ip1 = '172.16.0.10'
-    ip2 = '192.168.142.2'
-    port = 21
+# def main():
+#     ip1 = '172.16.0.10'
+#     ip2 = '192.168.142.2'
+#     port = 21
     
-    banner1 = retBanner(ip1, port)
-    if banner1:
-        print '[+] ' + ip1 + ': ' + banner1.strip('\n')
-        checkVulns(banner1)
-    banner2 = retBanner(ip2, port)
-    if banner2:
-        print '[+] ' + ip2 + ': ' + banner2.strip('\n')
-        checkVulns(banner2)
+def main():
+  portList = [21,22,25,80,110,443]
+  for x in range(1, 10):
+      ip = '192.168.95.' + str(x)
+      for port in portList:
+          print "checking " + ip + ': ' + str(port)
+          banner = retBanner(ip, port)
+          if banner:
+              print '[+] ' + ip + ': ' + banner
+              checkVulns(banner)
 if __name__ == '__main__':
-    main()
+  main()
+    
+#     banner1 = retBanner(ip1, port)
+#     if banner1:
+#         print '[+] ' + ip1 + ': ' + banner1.strip('\n')
+#         checkVulns(banner1)
+#     banner2 = retBanner(ip2, port)
+#     if banner2:
+#         print '[+] ' + ip2 + ': ' + banner2.strip('\n')
+#         checkVulns(banner2)
+# if __name__ == '__main__':
+#     main()
         
     
 
