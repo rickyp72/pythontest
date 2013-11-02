430446a8159b04a372fb9429a8a7f8671c832504
ricky <rickyp72@googmail.com>
2013-11-02 20:00:09 +0000

    updated

--- bannergrabber.py
+++ bannergrabber.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 import socket
+import os
+import sys
 def retBanner(ip, port):
     try:
         socket.setdefaulttimeout(2)
@@ -10,56 +12,38 @@ def retBanner(ip, port):
     except:
         return
         
-def checkVulns(banner):
-    f = open("vuln_banners.txt",'r')
+def checkVulns(banner, filename):
+    f = open(filename,'r')
     for line in f.readlines():
         if line.strip('\n') in banner:
-            print "[+] Server is vulnerable: "+banner.strip('\n'')
-#         
-# def checkVulns(banner):
-#      if ("FreeFloat Ftp Server (Version 1.00)" in banner):
-#          print "[+] FreeFloat FTP Server is vulnerable."
-#      elif ("/tnftpd/ " in banner):
-#          print "[+] tnftpd FTP Server is vulnerable."
-#      else:
-#          print "[-] FTP Server isn't vulnerable."       
-#      return 
-        
-# def main():
-#     ip1 = '172.16.0.10'
-#     ip2 = '192.168.142.2'
-#     port = 21
+            print "[+] Server is vulnerable: "+banner.strip('\n')   
+
     
 def main():
-  portList = [21,22,25,80,110,443]
-  for x in range(1, 11):
-      ip = '172.16.0.' + str(x)
-      for port in portList:
-          print "checking " + ip + ': ' + str(port)
-          banner = retBanner(ip, port)
-          if banner:
-              print '[+] ' + ip + ': ' + banner
-              checkVulns(banner)
+    if len(sys.argv) == 2:
+        filename = sys.argv[1]
+        print 'checking file exists'
+        if not os.path.isfile(filename):
+            print '[-] ' + filename + ' does not exist.'
+            exit(0)
+        print "File exits\n"
+        print 'checking access'
+        if not os.access(filename, os.R_OK):
+            print '[-] ' + filename + ' access denied.'
+            exit(0)
+        print "Access good\n"
+    else:
+        print '[-] Usage: ' + str(sys.argv[0]) + ' <vuln filename>'
+        exit(0)
+    portList = [21,22,25,80,110,443]
+    for x in range(8, 11):
+        ip = '172.16.0.' + str(x)
+        for port in portList:
+            print "checking " + ip + ': ' + str(port)
+            banner = retBanner(ip, port)
+            if banner:
+                print '[+] ' + ip + ': ' + banner
+                checkVulns(banner, filename)
 if __name__ == '__main__':
-  main()
+    main()
     
-#     banner1 = retBanner(ip1, port)
-#     if banner1:
-#         print '[+] ' + ip1 + ': ' + banner1.strip('\n')
-#         checkVulns(banner1)
-#     banner2 = retBanner(ip2, port)
-#     if banner2:
-#         print '[+] ' + ip2 + ': ' + banner2.strip('\n')
-#         checkVulns(banner2)
-# if __name__ == '__main__':
-#     main()
-        
-    
-
-
-
-# #    Error handling example
-# try:
-#     print "[+] 1337/0 = "+str(1337/0)
-# except Exception, e:
-#     print "[-] Error =  "+str(e)
